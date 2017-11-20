b27d58940ecc2132e73b46d951fb9bbd854eb1f0
Nicolas Drebenstedt <cutoffthetop@me.com>
2017-11-20 15:41:55 +0100

    Fix ws

--- scripts/install-as-root.sh
+++ scripts/install-as-root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
-id -u cloudplayer &>/dev/null || useradd -m -U -d /srv/cloudplayer cloudplayer
+id -u cloudplayer &>/dev/null || useradd -m -U -d /srv/cloudplayer cloudplayer
 apt-get update
 sudo apt-get install -y redis-server supervisor nginx

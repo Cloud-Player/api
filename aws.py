5645ee76edbd17555c18da544f0eef92f985486f
Nicolas Drebenstedt <cutoffthetop@me.com>
2017-11-20 18:42:37 +0100

    Add cloudplayer domain to allowed origins

--- aws.py
+++ aws.py
@@ -2,4 +2,4 @@ debug = False
 xheaders = True
 static_path = 'static'
 
-allowed_origins = 'http://localhost:8080, http://0.0.0.0:8080, http://127.0.0.1:8080'
+allowed_origins = 'https://cloud-player.io, http://localhost:8080, http://0.0.0.0:8080, http://127.0.0.1:8080'

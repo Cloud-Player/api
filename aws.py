ee7309e987d7b92c748de5a0f5f3cfa255969b3c
Nicolas Drebenstedt <cutoffthetop@me.com>
2017-12-11 16:10:16 +0100

    Update tornado config files

--- aws.py
+++ aws.py
@@ -2,4 +2,15 @@ debug = False
 xheaders = True
 static_path = 'static'
 
-allowed_origins = 'https://cloud-player.io, http://localhost:8080, http://0.0.0.0:8080, http://127.0.0.1:8080'
+allowed_origins = [
+    'https://cloud-player.io',
+    'http://localhost:8080',
+    'http://0.0.0.0:8080',
+    'http://127.0.0.1:8080']
+
+jwt_secret = 'secret'
+
+google_oauth = {
+    'key': 'key',
+    'secret': 'secret',
+    'redirect_uri': 'https://api.cloud-player.io/google'}
--- dev.py
+++ dev.py
@@ -2,4 +2,14 @@ debug = True
 xheaders = False
 static_path = 'static'
 
-allowed_origins = 'http://localhost:8080, http://0.0.0.0:8080, http://127.0.0.1:8080'
+allowed_origins = [
+    'http://localhost:8080',
+    'http://0.0.0.0:8080',
+    'http://127.0.0.1:8080']
+
+jwt_secret = 'secret'
+
+google_oauth = {
+    'key': 'key',
+    'secret': 'secret',
+    'redirect_uri': 'http://localhost:8040/google'}

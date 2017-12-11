863dbffb24f3e2c80ab83316f1426c9dd758afd9
Nicolas Drebenstedt <cutoffthetop@me.com>
2017-12-11 16:14:50 +0100

    Configure additional config values

--- src/cloudplayer/api/tests/conftest.py
+++ src/cloudplayer/api/tests/conftest.py
@@ -6,8 +6,9 @@ import cloudplayer.api.app
 
 @pytest.fixture(scope='session', autouse=True)
 def app():
-    opt.define('connect_timeout', type=int, default=3, group='httpclient')
-    opt.define('request_timeout', type=int, default=3, group='httpclient')
-    opt.define('max_redirects', type=int, default=1, group='httpclient')
-    opt.define('allowed_origins', type=str, default='*')
+    opt.define('connect_timeout', default=3, group='httpclient')
+    opt.define('request_timeout', default=3, group='httpclient')
+    opt.define('max_redirects', default=1, group='httpclient')
+    opt.define('google_oauth', default={'key': '', 'secret': ''}, group='app')
+    opt.define('allowed_origins', default=['*'], group='app')
     return cloudplayer.api.app.make_app()

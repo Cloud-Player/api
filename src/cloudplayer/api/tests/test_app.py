179e982fc8ef3e545d928f05d2c723a52210626e
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-01-12 11:57:27 +0100

    Try to fix travis 404 for missing user

--- src/cloudplayer/api/tests/test_app.py
+++ src/cloudplayer/api/tests/test_app.py
@@ -11,14 +11,14 @@ def test_base_route_should_return_404(http_client, base_url):
 
 @pytest.mark.gen_test
 def test_unsupported_method_should_return_405(http_client, base_url):
-    # request = tornado.httpclient.HTTPRequest(base_url, 'HEAD')
     response = yield http_client.fetch(
         base_url, method='HEAD', raise_error=False)
     assert response.code == 405
 
 
 @pytest.mark.gen_test
-def test_http_handler_should_set_default_headers(http_client, base_url):
+def test_http_handler_should_set_default_headers(
+        current_user, http_client, base_url):
     response = yield http_client.fetch(
         '{}/user/me'.format(base_url), raise_error=False)
     headers = dict(response.headers)

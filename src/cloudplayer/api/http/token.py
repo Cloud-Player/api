8f8c4a2b484af9fe64b4667ac22878801c8fb502
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-01-12 20:48:53 +0100

    Fix token handler spec

--- src/cloudplayer/api/http/token.py
+++ src/cloudplayer/api/http/token.py
@@ -6,18 +6,18 @@
     :license: GPL-3.0, see LICENSE for details
 """
 from cloudplayer.api.controller.token import TokenController
-from cloudplayer.api.http import HTTPHandler
 from cloudplayer.api.handler import EntityMixin, CollectionMixin
+from cloudplayer.api.http import HTTPHandler
 
 
 class Entity(EntityMixin, HTTPHandler):
 
     __controller__ = TokenController
 
-    SUPPORTED_METHODS = ('GET', 'PUT', 'OPTIONS')
+    SUPPORTED_METHODS = ('GET', 'PATCH', 'OPTIONS')
 
 
-class Collection(EntityMixin, HTTPHandler):
+class Collection(CollectionMixin, HTTPHandler):
 
     __controller__ = TokenController
 

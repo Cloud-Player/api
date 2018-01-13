4d7b080c59e12e880983c94cecca477aba0c02d9
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-01-13 18:14:10 +0100

    Rename playlist_item table

--- src/cloudplayer/api/tests/model/test_playlist_item.py
+++ src/cloudplayer/api/tests/model/test_playlist_item.py
@@ -7,7 +7,7 @@ import cloudplayer.api.model.base as model
 
 def test_playlist_item_model_should_create_table(db):
     playlist_item = sql.Table(
-        'playlist_item', model.Base.metadata, autoload=True,
+        'playlistitem', model.Base.metadata, autoload=True,
         autoload_with=db)
     assert playlist_item.exists(db.connection())
 

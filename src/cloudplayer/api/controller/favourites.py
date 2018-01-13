7d14f81dff0f2af1a629d2ccda0b214e0cce0a4c
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-01-13 19:21:53 +0100

    Add shortcut for current user favourites

--- src/cloudplayer/api/controller/favourites.py
+++ src/cloudplayer/api/controller/favourites.py
@@ -5,6 +5,7 @@
     :copyright: (c) 2017 by the cloudplayer team
     :license: GPL-3.0, see LICENSE for details
 """
+from cloudplayer.api.model.account import Account
 from cloudplayer.api.model.favourites import Favourites
 import cloudplayer.api.controller
 
@@ -13,3 +14,11 @@ class FavouritesController(cloudplayer.api.controller.Controller):
 
     __model__ = Favourites
     __policies__ = []
+
+    def read(self, ids):
+        if ids['id'] == 'mine':
+            provider_id = ids['provider_id']
+            account_id = self.current_user[provider_id]
+            account = self.db.query(Account).get((account_id, provider_id))
+            ids['id'] = account.favourites.id
+        return super().read(ids)

75e4d12c0768777115a6c4267c0629534cd336a3
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-01-25 12:30:18 +0100

    Add random shorthand id for playlists

--- src/cloudplayer/api/controller/playlist.py
+++ src/cloudplayer/api/controller/playlist.py
@@ -5,6 +5,8 @@
     :copyright: (c) 2017 by the cloudplayer team
     :license: GPL-3.0, see LICENSE for details
 """
+from  sqlalchemy.sql.expression import func
+
 from cloudplayer.api.model.playlist import Playlist
 from cloudplayer.api.policy import Owned
 import cloudplayer.api.controller
@@ -14,3 +16,12 @@ class PlaylistController(cloudplayer.api.controller.Controller):
 
     __model__ = Playlist
     __policies__ = [Owned]
+
+    def read(self, ids):
+        if ids['id'] == 'random':
+            random = self.db.query(
+                self.__model__).order_by(func.random()).first()
+            if random:
+                ids['id'] = random.id
+        entity = self.policy.read(self.__model__, ids)
+        return entity

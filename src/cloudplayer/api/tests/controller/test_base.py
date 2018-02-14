1e0e6466eb174cf2f8e3e0918ccda9f4aa6f9d62
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-02-14 21:16:51 +0100

    Fix signature

--- src/cloudplayer/api/tests/controller/test_base.py
+++ src/cloudplayer/api/tests/controller/test_base.py
@@ -12,7 +12,7 @@ def test_controller_should_store_creation_args(db, current_user):
 def test_controller_should_merge_ids_into_keywords():
     ids = {'a': 1, 'b': 2}
     kw = {'c': 3, 'd': 4}
-    params = Controller._merge_ids_with_kw(ids, **kw)
+    params = Controller._merge_ids_with_kw(ids, kw)
     assert params == {'a': 1, 'b': 2, 'c': 3, 'd': 4}
 
 
@@ -20,4 +20,4 @@ def test_controller_merge_should_complain_about_conflicting_fields():
     ids = {'a': 1, 'same': 'foo'}
     kw = {'b': 2, 'same': 'bar'}
     with pytest.raises(ControllerException):
-        Controller._merge_ids_with_kw(ids, **kw)
+        Controller._merge_ids_with_kw(ids, kw)

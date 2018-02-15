205fe224c114dcd13f89b565bdbdd6a17e3bccaa
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-02-15 18:01:38 +0100

    Export policy violation

--- src/cloudplayer/api/access/__init__.py
+++ src/cloudplayer/api/access/__init__.py
@@ -2,6 +2,6 @@ __import__('pkg_resources').declare_namespace(__name__)
 
 from .action import Anything, Create, Delete, Query, Read, Update
 from .fields import Available, Empty, Fields
-from .policy import Policy
+from .policy import Policy, PolicyViolation
 from .principal import Child, Everyone, Owner, Parent
 from .rule import Allow, Deny

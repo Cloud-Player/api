be14d2c615b747f5fd876b5e28e75ff9487c608e
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-03-10 14:23:59 +0100

    Infer current user from cookie

--- src/cloudplayer/api/controller/favourite_item.py
+++ src/cloudplayer/api/controller/favourite_item.py
@@ -5,10 +5,21 @@
     :copyright: (c) 2018 by Nicolas Drebenstedt
     :license: GPL-3.0, see LICENSE for details
 """
+import tornado.gen
+
+from cloudplayer.api.controller import Controller
 from cloudplayer.api.model.favourite_item import FavouriteItem
-import cloudplayer.api.controller
 
 
-class FavouriteItemController(cloudplayer.api.controller.Controller):
+class FavouriteItemController(Controller):
 
     __model__ = FavouriteItem
+
+    @tornado.gen.coroutine
+    def create(self, ids, kw, **params):
+        if 'account_provider_id' not in kw:
+            kw['account_provider_id'] = kw['favourite_provider_id']
+        if 'account_id' not in kw:
+            kw['account_id'] = self.accounts[kw['account_provider_id']]
+        entity = yield super().create(ids, kw, **params)
+        return entity

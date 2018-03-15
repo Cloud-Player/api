e99abaf0dc1f0fa2e8d4b8747aa9452bc4a563bf
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-03-15 16:03:00 +0100

    Drop convenience and require account properties

--- src/cloudplayer/api/controller/favourite_item.py
+++ src/cloudplayer/api/controller/favourite_item.py
@@ -5,8 +5,6 @@
     :copyright: (c) 2018 by Nicolas Drebenstedt
     :license: GPL-3.0, see LICENSE for details
 """
-import tornado.gen
-
 from cloudplayer.api.controller import Controller
 from cloudplayer.api.model.favourite_item import FavouriteItem
 
@@ -14,12 +12,3 @@ from cloudplayer.api.model.favourite_item import FavouriteItem
 class FavouriteItemController(Controller):
 
     __model__ = FavouriteItem
-
-    @tornado.gen.coroutine
-    def create(self, ids, kw, **params):
-        if 'account_provider_id' not in kw:
-            kw['account_provider_id'] = kw['favourite_provider_id']
-        if 'account_id' not in kw:
-            kw['account_id'] = self.accounts[kw['account_provider_id']]
-        entity = yield super().create(ids, kw, **params)
-        return entity

719ebaa04011f13aff266c2421baf2496fea5bbb
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-03-29 13:19:38 +0200

    Shift to from import

--- src/cloudplayer/api/controller/provider.py
+++ src/cloudplayer/api/controller/provider.py
@@ -5,10 +5,10 @@
     :copyright: (c) 2018 by Nicolas Drebenstedt
     :license: GPL-3.0, see LICENSE for details
 """
+from cloudplayer.api.controller import Controller
 from cloudplayer.api.model.provider import Provider
-import cloudplayer.api.controller
 
 
-class ProviderController(cloudplayer.api.controller.Controller):
+class ProviderController(Controller):
 
     __model__ = Provider

07139661ffa87b2b3af031909fdf6bc0b2a5dfc4
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-03-31 13:06:48 +0200

    Cover account socket unsubscription

--- src/cloudplayer/api/tests/ws/test_account.py
+++ src/cloudplayer/api/tests/ws/test_account.py
@@ -33,3 +33,34 @@ def test_account_entity_should_be_subscribable_over_websocket(
     ws_resp = yield ws_resp.connection.read_message()
     message = json.loads(ws_resp)
     assert message['body']['title'] == 'new title'
+
+
+@pytest.mark.xfail(strict=True)
+@pytest.mark.gen_test(timeout=3)
+def test_account_entity_should_be_unsubscribable_over_websocket(
+        user_push, user_fetch, db, account, monkeypatch):
+    monkeypatch.setattr(Entity, 'SUPPORTED_METHODS', ('GET', 'PATCH'))
+
+    message = {'channel': 'account.cloudplayer.{}'.format(account.id),
+               'method': 'SUB'}
+    ws_resp = yield user_push(message, keep_alive=True, await_reply=False)
+
+    http_resp = yield user_fetch(
+        '/account/cloudplayer/{}'.format(account.id),
+        method='PATCH', body={'title': 'new title'})
+    assert http_resp.code == 200
+
+    ws_message = yield ws_resp.connection.read_message()
+    message = json.loads(ws_message)
+    assert message['body']['title'] == 'new title'
+
+    message = {'channel': 'account.cloudplayer.{}'.format(account.id),
+               'method': 'UNSUB'}
+    yield ws_resp.connection.write_message(json.dumps(message))
+
+    http_resp = yield user_fetch(
+        '/account/cloudplayer/{}'.format(account.id),
+        method='PATCH', body={'title': 'other title'})
+    assert http_resp.code == 200
+
+    yield ws_resp.connection.read_message()

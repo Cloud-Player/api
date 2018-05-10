835cea9cf08158cafe6825614dbf2a3679663ca1
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-05-10 17:36:42 +0200

    Let the base controller handle ids/kw mismatches

--- src/cloudplayer/api/controller/favourite_item.py
+++ src/cloudplayer/api/controller/favourite_item.py
@@ -15,16 +15,3 @@ from cloudplayer.api.model.favourite_item import FavouriteItem
 class FavouriteItemController(Controller):
 
     __model__ = FavouriteItem
-
-    @tornado.gen.coroutine
-    def query(self, ids, kw):
-        # TODO: Resolve this workaround with dynamic parent resolution
-        favourite = self.db.query(Favourite).get(
-            (ids.pop('favourite_id'), ids.pop('favourite_provider_id')))
-        kw.pop('favourite_id', None)
-        kw.pop('favourite_provider_id', None)
-        if not favourite:
-            raise ControllerException(404, 'favourite not found')
-        kw['favourite'] = favourite
-        query = yield super().query(ids, kw)
-        return query
--- src/cloudplayer/api/controller/playlist_item.py
+++ src/cloudplayer/api/controller/playlist_item.py
@@ -20,15 +20,6 @@ class PlaylistItemController(Controller):
 
     @tornado.gen.coroutine
     def create(self, ids, kw, fields=Available):
-        # TODO: Resolve this workaround with dynamic parent resolution
-        playlist = self.db.query(Playlist).get(
-            (ids.pop('playlist_id'), ids.pop('playlist_provider_id')))
-        kw.pop('playlist_id', None)
-        kw.pop('playlist_provider_id', None)
-        if not playlist:
-            raise ControllerException(404, 'playlist not found')
-        kw['playlist'] = playlist
-
         track_id = kw.get('track_id')
         track_provider_id = kw.get('track_provider_id')
         track_controller = TrackController.for_provider(
@@ -37,6 +28,11 @@ class PlaylistItemController(Controller):
             'id': track_id, 'provider_id': track_provider_id})
         if not track:
             raise ControllerException(404, 'track not found')
+
+        playlist = self.db.query(Playlist).get(
+            (ids.pop('playlist_id'), ids.pop('playlist_provider_id')))
+        if not playlist:
+            raise ControllerException(404, 'playlist not found')
 
         if not playlist.image:
             playlist.image = track.image.copy()
@@ -47,13 +43,5 @@ class PlaylistItemController(Controller):
 
     @tornado.gen.coroutine
     def query(self, ids, kw):
-        # TODO: Resolve this workaround with dynamic parent resolution
-        playlist = self.db.query(Playlist).get(
-            (ids.pop('playlist_id'), ids.pop('playlist_provider_id')))
-        kw.pop('playlist_id', None)
-        kw.pop('playlist_provider_id', None)
-        if not playlist:
-            raise ControllerException(404, 'playlist not found')
-        kw['playlist'] = playlist
         query = yield super().query(ids, kw)
         return query.order_by(PlaylistItem.rank)
